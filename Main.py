98d930c2842d74f7c3677a2c97b2b518d1e0c741
Choi-Seungho <choiseungho1019@gmail.com>
2021-03-10 18:16:09 +0900

    Update reroll, Change random logic

--- Main.py
+++ Main.py
@@ -4,8 +4,26 @@ import pickle
 import random
 from textwrap import dedent
 
-def random_starting_pokemon(pokemon_list, sample_list, entity_list):
-    sample = random.sample(sample_list, 3)
+class Reroll(Button):
+    def __init__(self, pokemon_list, start_pokemon, entity_list):
+        super().__init__(parent = scene, model = 'quad', position=(0,-2), color = color.white, highlight_color = color.lime)
+        self.pokemon_list = pokemon_list
+        self.entity_list = entity_list
+        self.start_pokemon = start_pokemon
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                sample = random.sample(self.start_pokemon, 3)
+                for i in range(3):
+                    self.entity_list[i].texture = './resources/picture/small/{0:03d}.png'.format(sample[i])
+                    tooltip = Monster(self.pokemon_list[sample[i]-1]).get_tooltip()
+                    tool = Tooltip(text=tooltip)
+                    tool.wordwrap = 1
+                    tool.background = True
+                    self.entity_list[i].tooltip = tool
+
+def random_starting_pokemon(pokemon_list, start_pokemon, entity_list):
+    sample = random.sample(start_pokemon, 3)
     for i in range(3):
         entity_list[i].texture = './resources/picture/small/{0:03d}.png'.format(sample[i])
         tooltip = Monster(pokemon_list[sample[i]-1]).get_tooltip()
@@ -20,8 +38,6 @@ if __name__ == '__main__':
     start_pokemon = [1, 4, 7, 10, 13, 16, 19, 21, 23, 25, 27, 29, 32, 35, 37, 39, 41, 43, 46, 48, 
                      50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 74, 77, 79, 81, 83, 84, 86, 88, 90, 90,
                      92, 95, 96, 98, 100, 102, 104, 109, 111, 116, 118, 120, 129, 133, 138, 140, 147]
-    start_list = random.sample(start_pokemon, 3)
-    print(start_list)
     with open('./resources/pokemon_list.pkl', 'rb') as f:
         for i in range(1, 152):
             pokemon_list.append(pickle.load(f))
@@ -31,6 +47,6 @@ if __name__ == '__main__':
     start_pos = [5, 0, -5]
     for i in range(3):
         start_entity[i] = Button(parent = scene, model = 'quad', position=(start_pos[i],1.5), highlight_color = color.white)
-    
-    random_starting_pokemon(pokemon_list, start_list, start_entity)
+    reroll = Reroll(pokemon_list, start_pokemon, start_entity)
+    random_starting_pokemon(pokemon_list, start_pokemon, start_entity)
     app.run()
